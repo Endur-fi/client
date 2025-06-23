01efb87e63c2ba01ed20672f1522fae531099a8f
Hemant <hrsmart2150@gmail.com>
2025-06-23 20:06:07 +0530

    chore: revert changes of apollo-client.ts

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,5 +1,7 @@
 import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
 
+import { isMainnet } from "@/constants";
+
 const defaultOptions: DefaultOptions = {
   watchQuery: {
     fetchPolicy: "no-cache",
@@ -12,10 +14,10 @@ const defaultOptions: DefaultOptions = {
 };
 
 const apolloClient = new ApolloClient({
-  // uri: isMainnet()
-  //   ? "https://graphql.mainnet.endur.fi"
-  //   : "https://graphql.sepolia.endur.fi",
-  uri: "http://localhost:4000",
+  uri: isMainnet()
+    ? "https://graphql.mainnet.endur.fi"
+    : "https://graphql.sepolia.endur.fi",
+  // uri: "http://localhost:4000",
   cache: new InMemoryCache(),
   defaultOptions,
 });
