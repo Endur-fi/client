4bfa3ef8abd757833927eb54b3b54911247d5e9d
akiraonstarknet <zkdice@proton.me>
2025-12-16 17:47:20 +0530

    add season 2 points learn more link

--- src/components/season-points-card.tsx
+++ src/components/season-points-card.tsx
@@ -30,7 +30,7 @@ const SeasonPointsCard: React.FC<SeasonPointsCardProps> = ({ className }) => {
       </div>
       <div className="mt-3 flex w-full justify-center">
         <Link
-          href="#"
+          href="/rewards"
           className="w-full rounded-md bg-[#FFFFFF33] px-4 py-2 text-center text-xs font-medium text-white transition-all hover:bg-[#6BA89A]"
         >
           Learn More
