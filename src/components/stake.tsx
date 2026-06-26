a747a550b975095d441f2e4eae5827447d66dc7d
akcharlie24 <akshatbhardwaj24@gmail.com>
2026-06-26 16:20:42 +0530

    feat: remove lbtc staking

--- src/components/stake.tsx
+++ src/components/stake.tsx
@@ -603,29 +603,29 @@ const Stake: React.FC = () => {
     });
   }, [data, form, isPending]);
 
-  // if (lstConfig.SYMBOL === "LBTC") {
-  //   return (
-  //     <div className="relative flex h-full w-full flex-col gap-6">
-  //       <Stats
-  //         selectedPlatform={selectedPlatform}
-  //         getPlatformYield={getPlatformYield}
-  //         mode="stake"
-  //       />
-  //       <div className="rounded-[14px] border border-amber-600 bg-amber-50 px-4 py-3 text-sm text-[#6B7780]">
-  //         <span className="font-semibold text-[#1A1F24]">Note: </span>
-  //         LBTC is discontinued, we encourage users to withdraw their staked
-  //         assets. Learn more:{" "}
-  //         <Link
-  //           href="https://docs.endur.fi/docs/guides/how-to-stake-and-unstake-btc"
-  //           target="_blank"
-  //           className="text-blue-600 underline"
-  //         >
-  //           here
-  //         </Link>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  if (lstConfig.SYMBOL === "LBTC") {
+    return (
+      <div className="relative flex h-full w-full flex-col gap-6">
+        <Stats
+          selectedPlatform={selectedPlatform}
+          getPlatformYield={getPlatformYield}
+          mode="stake"
+        />
+        <div className="rounded-[14px] border border-amber-600 bg-amber-50 px-4 py-3 text-sm text-[#6B7780]">
+          <span className="font-semibold text-[#1A1F24]">Note: </span>
+          LBTC is discontinued, we encourage users to withdraw their staked
+          assets. Learn more:{" "}
+          <Link
+            href="https://docs.endur.fi/docs/guides/how-to-stake-and-unstake-btc"
+            target="_blank"
+            className="text-blue-600 underline"
+          >
+            here
+          </Link>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="relative flex h-full w-full flex-col gap-6">
@@ -1057,7 +1057,7 @@ const Stake: React.FC = () => {
 
       <div className="">
         {!address && (
-            <ConnectButton className="!w-full rounded-xl bg-[#17876D] py-6 text-sm font-semibold text-white hover:bg-[#17876D] disabled:bg-[#03624C4D] disabled:text-[#17876D] disabled:opacity-90" />
+          <ConnectButton className="!w-full rounded-xl bg-[#17876D] py-6 text-sm font-semibold text-white hover:bg-[#17876D] disabled:bg-[#03624C4D] disabled:text-[#17876D] disabled:opacity-90" />
         )}
 
         {address && (
